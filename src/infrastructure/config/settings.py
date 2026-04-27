7d5133a3d5cf26cbd1659e9290a9c8309b837d65
mehdibidjed <mehdibidjed25@gmail.com>
2026-04-27 22:20:52 +0100

    add seeting.py

--- src/infrastructure/config/settings.py
+++ src/infrastructure/config/settings.py
@@ -1,6 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import Literal
-
+import os
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",           # still useful for teammates
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
 
     # ==================== LLM ====================
     LLM_PROVIDER: Literal["deepseek", "gemini"] = "deepseek"
-    DEEPSEEK_API_KEY: str                          # ← your OS env var
+    DEEPSEEK_API_KEY: str | None = None                        # Pydantic reads from OS env var automatically
     DEEPSEEK_BASE_URL: str = "https://api.deepseek.com/v1"
     
     # Gemini fallback (your friend will use later)
